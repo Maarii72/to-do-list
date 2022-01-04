dcc81f883f950f0dbb1a245639cf4026b38fe7fe
Marilena Sales <marilena.sales@gmail.com>
2022-01-04 17:08:58 -0300

    javascript (refazer)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,50 +1,14 @@
-var input = document.querySelector("#tarefa");
-var tarefa = input.value;
+var input = document.getElementById("tarefa")
+var inputcheck = document.getElementById("checkbox")
+var ul = document.getElementById("ul");
+var item = document.getElementById("li");
 
 document.getElementById("submit").addEventListener("click",function listar(){
-document.getElementById("item").innerHTML = tarefa;
+  var li = document.createElement("li");
+  
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
 });
 
 
-/*
-var currentNumber = 0;
-
-document.getElementById("adicionar").addEventListener("click",function increment(){
-  currentNumber = currentNumber + 1;
-  
-  //voltar para cor se o numero for positivo
-  if(currentNumber >= 0){
-    document.getElementById("currentNumber").style.color='rgb(105, 89, 99)';
-  }
-
-  //desabilitar botão ao chegar no 10
-  if(currentNumber >= 10){
-    document.getElementById("adicionar").disabled = true;
-    document.getElementById("subtrair").disabled = false;
-  }
-  document.getElementById("currentNumber").innerHTML= currentNumber;
-});
-
-
-
-document.getElementById("subtrair").addEventListener("click",function increment(){
-  
-  currentNumber = currentNumber - 1;
-  
-
-  //mudar cor quando número for negativo
-  if (currentNumber < 0) {
-    document.getElementById("currentNumber").style.color='#B22222';
-  }
-  //fim da mudança de numero negativo
-
-  //desabilitar botão ao chegar no 0
-  if(currentNumber >= 0){
-    document.getElementById("subtrair").disabled = true;
-    document.getElementById("adicionar").disabled = false;
-  }
-  document.getElementById("currentNumber").innerHTML= currentNumber;
-
-  
-});
-*/
